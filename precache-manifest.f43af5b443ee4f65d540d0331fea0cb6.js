b69ebb95ebe45f93c97e6bf044bb9f195654ec9c
Irving Eslava <iaeg93@gmail.com>
2019-05-21 13:09:52 -0500

    Updates

---
+++ precache-manifest.f43af5b443ee4f65d540d0331fea0cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c64510093e5556f53cf4b6b29c02b2",
+    "url": "/cedula/index.html"
+  },
+  {
+    "revision": "225ae888543d07e2f5cf",
+    "url": "/cedula/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cedula/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "225ae888543d07e2f5cf",
+    "url": "/cedula/static/js/main.fc144735.chunk.js"
+  },
+  {
+    "revision": "1423d300ec2988d65a0c",
+    "url": "/cedula/static/js/runtime~main.e6465934.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cedula/static/media/logo.5d5d9eef.svg"
+  }
+]);
